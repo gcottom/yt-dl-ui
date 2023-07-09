0aa9896a504ab04fdd075c667f1b01ab7d00b635
gagecottom <gagecottom@hotmail.com>
2023-07-09 18:13:24 -0500

    fix a bad import

--- metaSearch.go
+++ metaSearch.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/gcottom/yt-dl-ui/musicbrainz"
+	"github.com/gcottom/musicbrainz"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 
 	"golang.org/x/oauth2/clientcredentials"
